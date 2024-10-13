4b58bea875c15ce955a1ddc147e843e63f6a41de
Diego Confessor <diegondonascimento@gmail.com>
2024-10-12 22:34:53 -0300

    integração

--- lojashoefy/app/cadastrarprodutos.tsx
+++ lojashoefy/app/cadastrarprodutos.tsx
@@ -176,6 +176,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: '#F8F8F8',
+    marginTop:30
   },
   container: {
     flex: 1,
--- lojashoefy/app/index.tsx
+++ lojashoefy/app/index.tsx
@@ -1,5 +1,5 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Image, StyleSheet, Text, StatusBar } from "react-native";  // Importando StatusBar
+import { Image, StyleSheet, Text, StatusBar, Pressable } from "react-native";  // Importando Pressable
 import { Button } from "../components/button";
 import { router } from "expo-router";
 
@@ -8,19 +8,30 @@ export default function Screen() {
         router.replace('/home');
     };
 
+    const goToCadastro = () => {
+        router.push('/cadastrarprodutos');  
+    };
+
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar 
                 barStyle="dark-content"  
                 backgroundColor="#ffffff"  
             />
+             <Pressable onPress={goToCadastro} style={styles.adminLink}>
+                <Text style={styles.adminText}>Área Administrativa</Text>
+            </Pressable>
+
             <Image
                 source={require('../assets/logo1.png')}
                 style={styles.logo}
                 resizeMode="cover"
             />
+
+           
+
             <Text style={styles.h1}>Loja-Shoefy</Text>
-            <Text style={styles.h2}>qualidade e Conforto.</Text>
+            <Text style={styles.h2}>Qualidade e Conforto.</Text>
             <Button
                 title="Compre Agora"
                 onPress={start}
@@ -36,10 +47,18 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#ffffff',
     },
+    adminLink: {
+        marginTop: 20,  // Espaço entre a imagem e o link
+    },
+    adminText: {
+        fontSize: 16,
+        color: '#000A3E',  // Cor do link
+        textDecorationLine: 'underline',
+    },
     logo: {
         width: '100%',
         height: 400,
-        marginBottom: 170
+        marginBottom: 20  // Ajuste do espaçamento entre a imagem e o restante do conteúdo
     },
     h1: {
         fontSize: 22,
